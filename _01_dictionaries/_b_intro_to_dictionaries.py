54caf13e53664093cee937277f5dd5e7298a2412
lvl0_module2 <aaritrj@gmail.com>
2024-07-19 18:28:25 -0700

    IP introToDict

--- _01_dictionaries/_b_intro_to_dictionaries.py
+++ _01_dictionaries/_b_intro_to_dictionaries.py
@@ -5,28 +5,30 @@ import unittest
 
 # TODO: Return a dictionary with 4 key-value pairs
 def initialize_dictionary():
-
-    return dict()
+    dictionary = { 1 : "First", 2 : 'Second', 3 : 'Third', 4 : 'Fourth'}
+    return dictionary
 
 # TODO: Return a dictionary using the two input parameter lists.
 #  The first element in both lists should be a key-value pair, same
 #  for the second, etc. Assume both input parameters have the same length.
 def initialize_dictionary_from_lists(keys, values):
-
-    return dict()
+    dictionary = {}
+    for key, value in zip(keys, values):
+        dictionary[key] = value
+    return dictionary
 
 # TODO: Return a dictionary with the specified key's value updated to the new
 #  value. If the key does not exist in the dictionary, do not change the
 #  dictionary.
 def dictionary_update(dictionary, key, new_value):
-
-    return dict()
+    dictionary[key] = new_value
+    return dictionary
 
 # TODO: Return a dictionary with the input dictionary's keys and values
 #  reversed.
 def reverse_key_values(dictionary):
-
-    return dict()
+    reversed_dict = {value:key for key, value in dictionary.items()}
+    return reversed_dict
 
 # TODO Return a dictionary containing the matching key-value pairs from
 #  dict_1 and dict_2:
